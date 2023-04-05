f6ac0ce405c79dd426af9a979b04c05fb84a1536
guzal <220930@astanait.edu.kz>
2023-04-05 16:10:48 +0600

    finding the a^n

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,9 @@ public class Main {
     public static void main(String[] args) {
         int n;
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the  a: ");
+        int a = scanner.nextInt();
+        System.out.print("Enter the  n: ");
         n = scanner.nextInt();
         int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
@@ -10,15 +13,17 @@ public class Main {
         }
 
         int min = findMinFromArr(arr, n);
-        System.out.println("minimum in the array " + min);
+        System.out.println("minimum in the array: " + min);
         double findAvg = findAvFromArr(arr);
-        System.out.println("avg of all elements is " + findAvg);
+        System.out.println("avg of all elements is: " + findAvg);
         boolean prime = checkPrime(n);
         System.out.println(prime);
         int findFac = factorial(n);
         System.out.println("factorial of n: " + findFac);
         int findFib= fibonacci(n);
         System.out.println(" n-th element in Fibonacci sequence is: " + findFib);
+        int findSqrt = sqrt(a, n);
+        System.out.println("a^n: " + findSqrt);
 
 
 
@@ -44,30 +49,28 @@ public class Main {
         return average;
     }
 
-    public static boolean checkPrime(int num) {
+    public static boolean checkPrime(int n) {
         {
-            boolean isprime = true;
-            Scanner input = new Scanner(System.in);
-            System.out.println("Enter the number:");
-            num = input.nextInt();
+            boolean isprime=true;
 
-            if (num < 2)
+
+            if (n < 2)
                 System.out.println("Number needs to be more than 1");
-            else if (num == 2)
+            else if (n == 2)
                 System.out.println("2 is a Prime number");
             else {
-                for (int divisor = 2; divisor <= (num / 2); divisor++) {
-                    if ((num % divisor) == 0) {
-                        isprime = false;
+                for (int divisor = 2; divisor <= (n / 2); divisor++) {
+                    if ((n % divisor) == 0) {
+                        isprime=false;
                         break;
                     }
                 }
                 if (isprime)
-                    System.out.printf("%d is a Prime number", num);
+                    System.out.printf("%d is a Prime number", n);
                 else
-                    System.out.printf("%d is a Composite number", num);
+                    System.out.printf("%d is a Composite number", n);
             }
-            input.close();
+
             return isprime;
         }
 
@@ -90,6 +93,15 @@ public class Main {
             return fibonacci(n-1) + fibonacci(n-2);
         }
     }
+
+    public static int sqrt(int a, int n) {
+        int res = 1;
+        for (int i = 0; i < n; i++) {
+            res *= a;
+        }
+        return res;
+    }
+
 
 
 
