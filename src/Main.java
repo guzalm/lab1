604f4015089f5575f36ca73c0f742d1a215c775d
guzal <220930@astanait.edu.kz>
2023-04-05 15:54:12 +0600

    finding the n-th element in Fibonacci sequence

--- src/Main.java
+++ src/Main.java
@@ -8,14 +8,20 @@ public class Main {
         for (int i = 0; i < n; i++) {
             arr[i] = scanner.nextInt();
         }
+
         int min = findMinFromArr(arr, n);
         System.out.println("minimum in the array " + min);
-        double findAvg = findAvFromArr(arr, n);
+        double findAvg = findAvFromArr(arr);
         System.out.println("avg of all elements is " + findAvg);
-        boolean prime = checkPrime(arr);
+        boolean prime = checkPrime(n);
         System.out.println(prime);
-        int result = (int) (n * factorial(n - 1));
-        System.out.println("Factorial of num is " + result);
+        int findFac = factorial(n);
+        System.out.println("factorial of n: " + findFac);
+        int findFib= fibonacci(n);
+        System.out.println(" n-th element in Fibonacci sequence is: " + findFib);
+
+
+
 
     }
 
@@ -28,7 +34,7 @@ public class Main {
         }
     }
 
-    public static double findAvFromArr(int[] arr, int n) {
+    public static double findAvFromArr(int[] arr) {
         double sum = 0;
         for (int i = 0; i < arr.length; i++) {
             sum = sum + arr[i];
@@ -38,47 +44,57 @@ public class Main {
         return average;
     }
 
-    public static boolean checkPrime(int[] arr) {
-        boolean isprime = true;
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter the number:");
-        int num = input.nextInt();
-        input.close();
+    public static boolean checkPrime(int num) {
+        {
+            boolean isprime = true;
+            Scanner input = new Scanner(System.in);
+            System.out.println("Enter the number:");
+            num = input.nextInt();
 
-        if (num < 1)
-            System.out.println("Number needs to be more than 1");
-        else if (num == 1)
-            System.out.println("1 is neither Prime nor Composite");
-        else {
-            for (int divisor = 2; divisor <= (num / 2); divisor++) {
-                if ((num % divisor) == 0) {
-                    isprime = false;
-                    break;
+            if (num < 2)
+                System.out.println("Number needs to be more than 1");
+            else if (num == 2)
+                System.out.println("2 is a Prime number");
+            else {
+                for (int divisor = 2; divisor <= (num / 2); divisor++) {
+                    if ((num % divisor) == 0) {
+                        isprime = false;
+                        break;
+                    }
                 }
+                if (isprime)
+                    System.out.printf("%d is a Prime number", num);
+                else
+                    System.out.printf("%d is a Composite number", num);
             }
-            if (isprime)
-                System.out.printf("%d is a Prime number", num);
-            else
-                System.out.printf("%d is a Composite number", num);
-
-
+            input.close();
+            return isprime;
         }
-        return isprime;
-    }
 
-    public static int factorial(int num) {
-        if (num == 0) {
+
+    }
+    public static int factorial(int n) {
+        if (n == 0) {
             return 1;
         } else {
-            int result = (int) (num * factorial(num - 1));
+            int result = (int) (n * factorial(n - 1));
             return result;
         }
     }
+    public static int fibonacci(int n) {
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else {
+            return fibonacci(n-1) + fibonacci(n-2);
+        }
+    }
+
 
 
 
 }
-
 
 
 
